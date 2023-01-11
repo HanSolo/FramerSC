e47508c7ed87976f0fa0dd273f9888effd2b655f
Gerrit Grunwald <han.solo@mac.com>
2023-01-11 08:16:57 +0100

    Initial Commit

---
+++ FramerSC/FramerSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramerSCApp.swift
+//  FramerSC
+//
+//  Created by Gerrit Grunwald on 11.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FramerSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
